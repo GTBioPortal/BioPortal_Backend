c3229905e69720adff6e51731f64e0314c15f297
Matthew Fraschilla <fraschilla.matthew@gmail.com>
2019-02-20 12:31:43 -0500

    db

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import os
 
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
