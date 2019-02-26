e45ea4d8310dc0da72c882e76d6dcea2a3addc28
Matthew Fraschilla <fraschilla.matthew@gmail.com>
2019-02-26 09:02:10 -0500

    application table foreign key test

---
+++ models/Application.py
@@ -0,0 +1,14 @@
+import datetime
+
+from . import db
+
+
+class Application(db.Model):
+    __tablename__ = 'applications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    applicant = db.Column(db.Integer, db.ForeignKey('student.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=db.func.current_timestamp())
+
+    def __init__(self, applicant):
+        self.applicant = applicant
