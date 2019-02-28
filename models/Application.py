8a2cea108ce349267a2f45a00ef7d14f5ad616ec
Matthew Fraschilla <fraschilla.matthew@gmail.com>
2019-02-28 16:26:00 -0500

    typo

--- models/Application.py
+++ models/Application.py
@@ -7,7 +7,7 @@ class Application(db.Model):
     __tablename__ = 'applications'
 
     id = db.Column(db.Integer, primary_key=True)
-    applicant = db.Column(db.Integer, db.ForeignKey('student.id'))
+    applicant = db.Column(db.Integer, db.ForeignKey('students.id'))
     timestamp = db.Column(db.DateTime, index=True, default=db.func.current_timestamp())
 
     def __init__(self, applicant):
