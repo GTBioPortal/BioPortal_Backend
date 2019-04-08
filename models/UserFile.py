ca5f7e83a4a4413e8dfc6ee52bf380b309a5ef78
Matthew Fraschilla <fraschilla.matthew@gmail.com>
2019-04-08 11:24:27 -0400

    changed to multipart s3 upload

--- models/UserFile.py
+++ models/UserFile.py
@@ -40,6 +40,7 @@ class UserFile(db.Model):
                 aws_secret_access_key=os.environ['S3_SECRET_KEY']
             )
             data = document.read()
-            s3.Bucket('gtbioportal').put_object(Key='fname2.pdf', Body=data)
+            s3.upload_fileobj(data, 'gtbioportal', 'documents/fname.pdf')
+            #s3.Bucket('gtbioportal').put_object(Key='fname2.pdf', Body=data)
         except Exception as e:
             raise e
