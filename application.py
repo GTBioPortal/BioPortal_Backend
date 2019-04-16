77d5be0d49b7f4fba975a79eed3620b5e83b9b18
Matthew Fraschilla <fraschilla.matthew@gmail.com>
2019-04-15 21:43:01 -0400

    Secure cookie test

--- application.py
+++ application.py
@@ -84,15 +84,17 @@ def get_all_jobs():
         })
         return response, 401
 
-@application.route('/cookie_test/', methods=['PUT'])
+@application.route('/cookie_test/', methods=['PUT', 'GET'])
 def cookie_test():
-    data = request.json
-    response = jsonify({
-        'status': 'success',
-        'data': 'testing'
-    })
-    response.set_cookie('jwt', 'token_val', max_age=7200, httponly=True)
-    return response, 200
+    if request.method == 'GET':
+        response = jsonify({
+            'status': 'success',
+            'data': 'testing'
+        })
+        response.set_cookie('jwt', 'token_val', max_age=7200, httponly=True)
+        return response, 200
+    else:
+        print(request.cookies)
 
 @application.route('/jobs/<job_id>', methods=['GET', 'PUT'])
 def get_job(job_id):
